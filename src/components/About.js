ef5b497b19f28b60521d62ebe601296d66d66141
annastacia <annastaciangina96@gmail.com>
2022-07-20 08:48:52 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { image } from "../data/data";
 
 function About() {
   return (
-    <div id ="about">About
+    <div id ="about">
     <h2>About Me</h2>
     <p> Learning step by step</p>
     <img scr={image} alt="I made this"/>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,10 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
+  const style={color:"firebrick"}
   return( 
     <div id="home"> Home
-    <h1 style={{color:'firebrick'}}> Your{name} is a Web Developer from Your {city}</h1>
+    <h1 style={style}> Your{name} is a Web Developer from Your {city}</h1>
   </div>
   );
 }
